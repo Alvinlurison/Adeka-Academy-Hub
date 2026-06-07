7c8db0686cd12be5273bb29ef4176457d4f08d6b
Alvinlurison <alvinlurison@gmail.com>
2026-06-07 13:51:51 +0000

    Add firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+getAuth,
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyBmsSCbPmXCk4nPZAXKfZVZrjEn9x_cwGA",
+authDomain: "adecomm-hub.firebaseapp.com",
+projectId: "adecomm-hub",
+storageBucket: "adecomm-hub.firebasestorage.app",
+messagingSenderId: "771891537050",
+appId: "1:771891537050:web:5dae9122bb30513f85ab23"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+window.auth = auth;
+window.createUserWithEmailAndPassword = createUserWithEmailAndPassword;
+window.signInWithEmailAndPassword = signInWithEmailAndPassword;
