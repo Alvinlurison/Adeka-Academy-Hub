12a7d4f99db0b62b5056e287fe1f9dae4be279ce
Alvinlurison <alvinlurison@gmail.com>
2026-06-08 17:37:55 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -25,6 +25,3 @@ window.auth = auth;
 window.createUserWithEmailAndPassword = createUserWithEmailAndPassword;
 window.signInWithEmailAndPassword = signInWithEmailAndPassword;
 window.onAuthStateChanged = onAuthStateChanged;
-
-alert("Firebase Config Loaded");
-alert("onAuthStateChanged = " + typeof onAuthStateChanged);
