cecd72e7942e85ab527f7708661c4c0894aa2353
Alvinlurison <alvinlurison@gmail.com>
2026-06-09 14:47:05 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,17 @@
-import { initializeApp } from 
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
-getAuth,
-createUserWithEmailAndPassword,
-signInWithEmailAndPassword,
-onAuthStateChanged,
-signOut 
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 const firebaseConfig = {
 apiKey: "AIzaSyBmsSCbPmXCk4nPZAXKfZVZrjEn9x_cwGA",
 authDomain: "adecomm-hub.firebaseapp.com",
@@ -21,9 +24,14 @@ appId: "1:771891537050:web:5dae9122bb30513f85ab23"
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-
 window.auth = auth;
+const db = getFirestore(app);
+
 window.createUserWithEmailAndPassword = createUserWithEmailAndPassword;
 window.signInWithEmailAndPassword = signInWithEmailAndPassword;
 window.onAuthStateChanged = onAuthStateChanged;
 window.signOut = signOut;
+window.db = db;
+window.doc = doc;
+window.setDoc = setDoc;
+window.getDoc = getDoc;
